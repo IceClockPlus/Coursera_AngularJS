975261e26915b8947e2f4d43099939b05e4f0c30
unknown <aravenapro98@gmail.com>
2021-05-27 02:08:58 -0400

    First implementation of the function to check the list

--- module1-solution/app.js
+++ module1-solution/app.js
@@ -3,5 +3,35 @@
     angular.module('LunchCheck',[])
     .controller('LunchCheckController',LunchCheckController);
 
-
-})();
+    LunchCheckController.$inject= ['$scope'];
+    function LunchCheckController($scope){
+        $scope.items = "";
+        $scope.numberItems =0;
+        $scope.message = "";
+        
+        $scope.checkList = function(){
+            if($scope.items  !== ""){
+                var list = $scope.items.split(',');
+            $scope.numberItems = list.length;
+            }else{
+                $scope.numberItems =0;
+            }
+            
+            $scope.message = changeMessage($scope.numberItems);
+        };
+        
+        function changeMessage(numberItems){
+            if (numberItems == 0){
+                return "Please enter data first should show up. 'Empty";
+            }
+            else if (numberItems > 0 && numberItems <= 3){
+                return "Enjoy!";
+            }else{
+                return "Too much!";
+            }
+        }
+       
+        
+    }
+})
+();
