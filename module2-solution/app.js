8ceb1c5cdc7aed2a28fb05ffc932d15d9c5cd6c8
unknown <aravenapro98@gmail.com>
2021-06-08 18:00:18 -0400

    Declared controlers as syntax in index.html

--- module2-solution/app.js
+++ module2-solution/app.js
@@ -1,5 +1,9 @@
 (function(){
-    
+    'use strict';
+    angular.module('ShoppingListCheckOff',[])
+    .controller('ToBuyController',ToBuyController)
+    .controller('AlreadyBoughtController',AlreadyBoughtController)
+    .service('ShoppingListCheckOffService',ShoppingListCheckOffService)
 
 
 })();
