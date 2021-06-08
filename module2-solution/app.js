0e56e471c3e37b62381268165f62d11a36912e43
unknown <aravenapro98@gmail.com>
2021-06-08 19:03:02 -0400

    Corrections on ToBuyController

--- module2-solution/app.js
+++ module2-solution/app.js
@@ -3,23 +3,57 @@
     angular.module('ShoppingListCheckOff',[])
     .controller('ToBuyController',ToBuyController)
     .controller('AlreadyBoughtController',AlreadyBoughtController)
-    .service('ShoppingListCheckOffService',ShoppingListCheckOffService)
+    .service('ShoppingListCheckOffService',ShoppingListCheckOffService);
 
     ToBuyController.$inject = ['ShoppingListCheckOffService'];
     function ToBuyController(ShoppingListCheckOffService){
-
+        var buyCtrl = this;
+        buyCtrl.buyList = ShoppingListCheckOffService.getToBuyList();
     }
 
     AlreadyBoughtController.$inject  = ['ShoppingListCheckOffService'];
     function AlreadyBoughtController(ShoppingListCheckOffService){
-
+        var boughtCtrl = this;
+        boughtCtrl.boughtList = ShoppingListCheckOffService.getBoughtList();
     }
 
     function ShoppingListCheckOffService(){
         var service = this;
 
+        var toBuyList = [
+            {
+                name : "Cookies",
+                quantity : 20   
+            },
+            {
+                name : "Sausage",
+                quantity : 15
+            },
+            {
+                name : "Apple",
+                quantity:9
+            },
+            {
+                name : "Brownies",
+                quantity : 11
+            },
+            {
+                name : "Chips",
+                quantity : 8
+            }
+        ]
 
+        var boughtList = [];
+
+        service.getToBuyList = function(){
+            return toBuyList;
+        }
+
+        service.getBoughtList = function(){
+            return boughtList;
+        }
 
     }
 
 })();
+
