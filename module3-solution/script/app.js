6a17973286e788a9d813dad4b890215559899af1
unknown <aravenapro98@gmail.com>
2021-06-13 23:42:30 -0400

    Created MenuSearchService in module3

--- module3-solution/script/app.js
+++ module3-solution/script/app.js
@@ -2,10 +2,17 @@
     'use strict';
     angular.module('NarrowItDownApp',[])
     .controller('NarrowItDownController',NarrowItDownController)
+    .service('MenuSearchService',MenuSearchService)
+    .constant('ApiBasePath',"https://davids-restaurant.herokuapp.com/")
 
 
     function NarrowItDownController(){
-        
+
+    }
+
+    MenuSearchService.$inject = ['$http','ApiBasePath'];
+    function MenuSearchService($http,ApiBasePath){
+
     }
 
 })();
