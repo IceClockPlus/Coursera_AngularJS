038a58303bbc0b9c736198266c3abe5686b1fabb
unknown <aravenapro98@gmail.com>
2021-06-14 01:23:47 -0400

    MenuSearchService completed and implemented in NarrowItDownController

--- module3-solution/script/app.js
+++ module3-solution/script/app.js
@@ -6,13 +6,54 @@
     .constant('ApiBasePath',"https://davids-restaurant.herokuapp.com/")
 
 
-    function NarrowItDownController(){
+    NarrowItDownController.$inject = ['MenuSearchService']
+    function NarrowItDownController(MenuSearchService){
+        var narrowCtrl = this;
 
+        narrowCtrl.getMatchedMenuItems = function(searchTerm){
+            //Check if the string contains a value
+            if(searchTerm){
+                var promise = MenuSearchService.getMatchedMenuItems(searchTerm);
+                promise.then(function(response){
+                    narrowCtrl.found = response.data;
+
+                })
+
+            }else{
+                narrowCtrl.found = [];
+            }
+        }
     }
 
+
+    /**SERVICES**/
     MenuSearchService.$inject = ['$http','ApiBasePath'];
     function MenuSearchService($http,ApiBasePath){
+        var service = this;
 
+        this.getMatchedMenuItems = function(searchTerm){
+            return $http({
+                method : "GET",
+                url : (ApiBasePath + "/menu_items.json")
+            }).then(function(response){
+
+                var foundItems = [];
+                console.log(response.data.menu_items);
+                console.log(response.data.menu_items.length);
+
+                var menulength = response.data.menu_items.length;
+                for(var i = 1; i < menulength ; i++ ){
+                    //Get the item from the response
+                    var item = response.data.menu_items[i];
+                    //Check if the description matches with string given
+                    if(item.description.indexOf(searchTerm.toLowerCase()) !== -1 ){
+                        foundItems.push(item);
+                    }
+                }
+                console.log(foundItems);
+                return foundItems;
+            });
+        }
     }
 
 })();
