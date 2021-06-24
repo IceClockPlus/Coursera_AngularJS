f990e84a2f29e055a5720db2309ada4ca80362a6
unknown <aravenapro98@gmail.com>
2021-06-24 02:14:46 -0400

    categorytemplate created and getallcategory method developed

--- module4-solution/src/menudata.service.js
+++ module4-solution/src/menudata.service.js
@@ -1,14 +1,22 @@
 (function(){
     'use strict';
     angular.module('data')
-    .service('MenuDataService',MenuDataService);
+    .service('MenuDataService',MenuDataService)
+    .constant('ApiBasePath','https://davids-restaurant.herokuapp.com/');
 
-    MenuDataService.$inject = ['$http'];
-    function MenuDataService($http){
+    MenuDataService.$inject = ['$http','ApiBasePath'];
+    function MenuDataService($http,ApiBasePath){
         var serv = this;
 
         serv.getAllCategory = function(){
-
+            return $http({
+                method: "GET",
+                url: (ApiBasePath + 'categories.json')
+            }).then(function(response){
+                console.log(response);
+                var categories = response.data;
+                return categories;
+            });
         }
 
         serv.getItemsForCategory = function(categoryShortName){
